ae118d2a4c134651d1c691ed7fdef4227b6d6d9c
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-10-01 23:40:14 -0400

    db.json_created

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Header = () => {
+
+
+    return (
+        <>
+        <header>
+            <h1>My SuperHERO</h1>
+            <nav>
+                
+            </nav>
+        </header>
+        </>
+    )
+} 
